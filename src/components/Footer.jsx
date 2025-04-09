44a10a75f5a27f9dba304646dccd9380415f2cae
Artem Posudevskyi <penquin4m@gmail.com>
2025-04-10 01:38:32 +0300

    added links to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ const Footer = () => {
           <div className="text-5xl font-bold logoFont">Movie Space</div>
 
           <div className="flex interFont space-x-4 mt-4 md:mt-0">
-            <a href="#" className="hover:text-gray-400">LinkedIn</a>
-            <a href="#" className="hover:text-gray-400">Github</a>
-            <a href="#" className="hover:text-gray-400">Instagram</a>
+            <a href="https://www.linkedin.com/in/artem-posudevskyi-1aa826274/" className="hover:text-gray-400">LinkedIn</a>
+            <a href="https://github.com/4twerka" className="hover:text-gray-400">Github</a>
+            <a href="https://www.instagram.com/4twerka/" className="hover:text-gray-400">Instagram</a>
           </div>
 
           <div className="text-sm mt-4 md:mt-0 interFont">Made by Artem Posudevskyi</div>
